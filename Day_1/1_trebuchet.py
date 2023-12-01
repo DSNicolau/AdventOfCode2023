5b0ccfd6b6b81cd1fbb96a9acde34451cf8be77c
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-01 14:38:05 +0000

    Updated day 1 part 2

--- Day_1/1_trebuchet.py
+++ Day_1/1_trebuchet.py
@@ -30,16 +30,16 @@ numbers_dict = {  'one':'1',
 
 solution = 0
 for i in data:
-    new_string = i
     for key, value in numbers_dict.items():
-        if key in new_string:
+        if key in i:
             # Note: 'sevenine' is considered as 79
             # Substituting 'seven' per '7' and 'nine' per '9', would result in '7ine', which is not correct
             # Instead, a nice hack we do here is to substitute each number string by their first character, their number, and their last character
             # This would result in 's7n9e' :D
             key_list = [*key]
-            new_string = new_string.replace(key, key_list[0]+value+key_list[-1])
-    numbers = [int(j) for j in [*new_string] if j.isdigit()]
+            i = i.replace(key, key_list[0]+value+key_list[-1])
+    # Then we just repeat what've done previously
+    numbers = [int(j) for j in [*i] if j.isdigit()]
     solution += 10*numbers[0]+numbers[-1]
 
 print('Part 2 solution:',solution)
