3b022f47c4c0d5b03e4d31acbc8dcfdf1720aae1
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-03 17:59:09 +0000

    Day 3 Part 1 solved

--- Day_3/3_gear_ratios.py
+++ Day_3/3_gear_ratios.py
@@ -2,7 +2,7 @@
 Day 3: Gear Ratios
 Author: dnicolauit
 Date: 2023-12-03
-Answer 1: 
+Answer 1: 537832
 Answer 2: 
 """ 
 
@@ -14,22 +14,25 @@ data_matrix = []
 not_symbols = '0123456789.'
 
 #Get data
-data = open("Day_3/Examples/example8.txt").read().splitlines()
+data = open("Day_3/3_gear_ratios_data.txt").read().splitlines()
 data_dict={key: [] for key in range(0,len(data))}
 for i in range(len(data)):
     # Put our data in a matrix
     data_matrix.append([*data[i]])
-    # Put the found numbers in a dictionary with the respective indexes of the first digit
-    data_dict[i].append({int(m.group(0)):m.start(0) for m in re.finditer("\d+", data[i])})
+    # Put the found numbers in a dictionary as keys with the respective indexes of the first digit
+    data_dict[i]={key:[] for key in re.findall("\d+", data[i])}
+    for m in re.finditer("\d+", data[i]):
+        data_dict[i][str(int(m.group(0)))].append(m.start(0))
 
 solution = 0
 for i in range(len(data)):
-    for number in data_dict[i][0].keys():
+    for number in data_dict[i].keys():
         number_len = len(str(number))
-        index = data_dict[i][0][number]
-        use_number = see_if_adjacent(matrix=data_matrix, number_len=number_len, index_horizontal=index, index_vertical=i, restrictions=not_symbols)
-        if use_number:
-            solution += number
+        index = data_dict[i][number]
+        for j in index:
+            use_number = see_if_adjacent(matrix=data_matrix, number_len=number_len, index_horizontal=j, index_vertical=i, restrictions=not_symbols)
+            if use_number:
+                solution += int(number)
 
 print('Part 1 solution:',solution)
 
--- Day_3/utils.py
+++ Day_3/utils.py
@@ -5,6 +5,7 @@ def see_if_adjacent(matrix, number_len, index_horizontal, index_vertical, restri
     max_horizontal = len(matrix[0])
     vertical = [-1, 0, 1]
     neighbors = []
+    # Create the neighbors coordinates of our number
     for i in vertical:
         neighbors.append((index_vertical+i, index_horizontal-1))
         neighbors.append((index_vertical+i, index_horizontal+number_len))
@@ -13,7 +14,9 @@ def see_if_adjacent(matrix, number_len, index_horizontal, index_vertical, restri
         neighbors.append((index_vertical+1, index_horizontal+i))
 
     for coord in neighbors:
+        # Check if the coordinates are in the matrix
         if 0<=coord[0]<max_vertical and 0<=coord[1]<max_horizontal:
+            # Check if the coordinates are not either a number or a symbol
             if  matrix[coord[0]][coord[1]] not in restrictions:
                 return True
     return False
