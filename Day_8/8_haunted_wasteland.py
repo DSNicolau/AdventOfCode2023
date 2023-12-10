f0f6e971d1e404554619a2822e0b6710eb26c992
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-10 23:53:54 +0000

    Day 8 part 2

--- Day_8/8_haunted_wasteland.py
+++ Day_8/8_haunted_wasteland.py
@@ -3,9 +3,9 @@ Day 8: Haunted Wasteland
 Author: dnicolauit
 Date: 2023-12-10
 Answer 1: 19637
-Answer 2: 
+Answer 2: 8811050362409
 """ 
-
+import utils
 data = open("Day_8/8_haunted_wasteland_data.txt").read()
 
 # Part 1
@@ -30,3 +30,35 @@ while current_node != 'ZZZ':
     else: i += 1
     solution += 1
 print('Part 1 solution:',solution)
+
+
+# Part 2
+current_node = []
+final_node = []
+for key, _ in nodes.items():
+    if 'A' in key:
+        current_node.append(key)
+    elif 'Z' in key:
+        final_node.append(key)
+
+numbers = []
+solution = 0
+for node in current_node:
+    i = 0
+    number = 0
+    while node not in final_node:
+        node = nodes[node][directions[i]]
+        if i == len(directions)-1:
+            i = 0
+        else: i += 1
+        number += 1
+    numbers.append(number)
+
+num1 = numbers[0]
+num2 = numbers[1]
+solution = utils.find_lcm(num1, num2)
+
+for i in range(2, len(numbers)):
+    solution = utils.find_lcm(solution, numbers[i])
+
+print('Part 2 solution:',solution)
